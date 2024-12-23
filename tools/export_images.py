6df42e87318a86c11f07239d5d1a40575d3141e9
kabirz <jxwazxzhp@126.com>
2024-12-23 12:05:39 +0800

    tools: update export images

--- tools/export_images.py
+++ tools/export_images.py
@@ -10,20 +10,19 @@ from west.util import west_topdir
 TOP_DIR = Path(west_topdir())
 TOOL_DIR = TOP_DIR / 'apps/tools'
 BUILD_DIR = TOP_DIR / 'build'
+MCUBOOT_BIN = BUILD_DIR / 'mcuboot/zephyr/zephyr.bin'
+APP_SIGN_BIN = BUILD_DIR / 'data_collect/zephyr/zephyr.signed.bin'
+OUT_APP = BUILD_DIR / 'app.bin'
 
 
-def generate_app():
-    MCUBOOT_BIN = BUILD_DIR / 'mcuboot/zephyr/zephyr.bin'
-    APP_SIGN_BIN = BUILD_DIR / 'data_collect/zephyr/zephyr.signed.bin'
-    OUT_APP = BUILD_DIR / 'app.bin'
-
+def generate_app() -> None:
     with OUT_APP.open(mode='wb') as f:
         f.write(MCUBOOT_BIN.read_bytes())
         f.seek(128 * 1024, os.SEEK_SET)
         f.write(APP_SIGN_BIN.read_bytes())
 
 
-def zip_files(files: List[Tuple[Path, str]], out_zip_file: Path):
+def zip_files(files: List[Tuple[Path, str]], out_zip_file: Path) -> None:
     with zipfile.ZipFile(out_zip_file, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for file, path in files:
             zipf.write(file, os.path.join(path, os.path.basename(file)))
@@ -32,8 +31,8 @@ def zip_files(files: List[Tuple[Path, str]], out_zip_file: Path):
 
 
 files: List[Tuple[Path, str]] = [
-    (BUILD_DIR / 'app.bin', 'images'),
-    (BUILD_DIR / 'data_collect/zephyr/zephyr.signed.bin', 'images'),
+    (OUT_APP, 'images'),
+    (APP_SIGN_BIN, 'images'),
     (TOOL_DIR / 'parser_raw.py', 'tools'),
     (TOOL_DIR / 'udp_multi_getinfo.py', 'tools'),
     (TOOL_DIR / 'udp_multi_setinfo.py', 'tools'),
