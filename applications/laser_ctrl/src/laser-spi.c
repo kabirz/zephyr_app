4f1da9f1ae1eb82cca9fe93b274afc3e201f344f
kabirz <jxwazxzhp@126.com>
2025-07-09 16:31:05 +0800

    laser: fpga add cmd
    Signed-off-by: kabirz <jxwazxzhp@126.com>

--- applications/laser_ctrl/src/laser-spi.c
+++ applications/laser_ctrl/src/laser-spi.c
@@ -5,59 +5,100 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(laser_spi, LOG_LEVEL_INF);
 
+#define VERSION_GET_REG 0x00
+#define XAXIS_GET_REG   0x04
+#define YAXIS_GET_REG   0x08
+
 static const struct spi_dt_spec spi_spec =
 	SPI_DT_SPEC_GET(DT_NODELABEL(spi_laser_fpga), SPI_WORD_SET(8), 0);
 
-#ifdef CONFIG_SHELL
-#include <zephyr/shell/shell.h>
-static int cmd_version_get(const struct shell *ctx, size_t argc, char **argv)
+static int spi_tranfer(uint8_t *tx_buffer, uint8_t *rx_buffer, size_t size)
 {
-	return 0;
-}
-
-static int cmd_spi_transceive(const struct shell *ctx, size_t argc, char **argv)
-{
-	uint8_t rx_buffer[8] = {0};
-	uint8_t tx_buffer[8] = {0};
-
-	int bytes_to_send = argc - 1;
-
-	for (int i = 0; i < bytes_to_send; i++) {
-		tx_buffer[i] = strtol(argv[1 + i], NULL, 16);
-	}
-
-	const struct spi_buf tx_buffers = {.buf = tx_buffer, .len = bytes_to_send};
-	const struct spi_buf rx_buffers = {.buf = rx_buffer, .len = bytes_to_send};
+	const struct spi_buf tx_buffers = {.buf = tx_buffer, .len = size};
+	const struct spi_buf rx_buffers = {.buf = rx_buffer, .len = size};
 
 	const struct spi_buf_set tx_buf_set = {.buffers = &tx_buffers, .count = 1};
 	const struct spi_buf_set rx_buf_set = {.buffers = &rx_buffers, .count = 1};
 
-	int ret = spi_transceive_dt(&spi_spec, &tx_buf_set, &rx_buf_set);
+	return spi_transceive_dt(&spi_spec, &tx_buf_set, &rx_buf_set);
+}
 
-	if (ret < 0) {
-		shell_error(ctx, "spi_transceive returned %d", ret);
-		return ret;
+int fpga_version_get(void)
+{
+	uint8_t rx_buf[5] = {0}, tx_buf[5] = {0};
+
+	tx_buf[0] = VERSION_GET_REG;
+	if (spi_tranfer(tx_buf, rx_buf, sizeof(rx_buf))) {
+		LOG_ERR("spi tranfer error");
+		return -1;
+	} else {
+		LOG_HEXDUMP_INF(rx_buf, sizeof(rx_buf), "version");
 	}
+	return 0;
+}
 
-	shell_print(ctx, "TX:");
-	shell_hexdump(ctx, tx_buffer, bytes_to_send);
+int Xaxis_data_get(void)
+{
+	uint8_t rx_buf[9] = {0}, tx_buf[9] = {0};
 
-	shell_print(ctx, "RX:");
-	shell_hexdump(ctx, rx_buffer, bytes_to_send);
+	tx_buf[0] = XAXIS_GET_REG;
+	if (spi_tranfer(tx_buf, rx_buf, sizeof(rx_buf))) {
+		LOG_ERR("spi tranfer error");
+		return -1;
+	} else {
+		LOG_HEXDUMP_INF(rx_buf, sizeof(rx_buf), "Xaxis");
+	}
+	return 0;
+}
 
-	return ret;
+int Yaxis_data_get(void)
+{
+	uint8_t rx_buf[9] = {0}, tx_buf[9] = {0};
+
+	tx_buf[0] = YAXIS_GET_REG;
+	if (spi_tranfer(tx_buf, rx_buf, sizeof(rx_buf))) {
+		LOG_ERR("spi tranfer error");
+		return -1;
+	} else {
+		LOG_HEXDUMP_INF(rx_buf, sizeof(rx_buf), "Yaxis");
+	}
+	return 0;
+}
+
+#ifdef CONFIG_SHELL
+#include <zephyr/shell/shell.h>
+static int shell_version_get(const struct shell *ctx, size_t argc, char **argv)
+{
+	fpga_version_get();
+	return 0;
+}
+static int shell_Xaxis_get(const struct shell *ctx, size_t argc, char **argv)
+{
+	Xaxis_data_get();
+	return 0;
+}
+
+static int shell_Yaxis_get(const struct shell *ctx, size_t argc, char **argv)
+{
+	Yaxis_data_get();
+	return 0;
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_biss_cmds,
 			       SHELL_CMD_ARG(version, NULL,
 					     "get biss version\n"
 					     "Usage: version",
-					     cmd_version_get, 2, 0),
-			       SHELL_CMD_ARG(transfer, NULL,
-					     "transfer bytes to an spi device\n"
-					     "Usage: transfer <reg> [<byte1>, ...]",
-					     cmd_spi_transceive, 2, 7),
+					     shell_version_get, 2, 0),
+			       SHELL_CMD_ARG(xaxis, NULL,
+					     "get x axis data\n"
+					     "Usage: xaxis",
+					     shell_Xaxis_get, 2, 0),
+			       SHELL_CMD_ARG(yaxis, NULL,
+					     "get y axis data\n"
+					     "Usage: xaxis",
+					     shell_Yaxis_get, 2, 0),
 			       SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(biss, &sub_biss_cmds, "Spi biss commands", NULL);
 #endif
+
